b90a075dced9cf5b372ab6bb99c0072861320f1d
beefi-git <admin.it@beefi.io>
2020-08-19 12:00:44 +0800

    Test _static folder and misc changes

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'BFQ4004 Dual-band Wi-Fi Module AT Command Reference'
+project = 'BeeFi BFQ4004 Dual-band Wi-Fi Module'
 copyright = '2020, BeeFi Technologies Inc.'
-author = 'Job'
+author = 'John Yu'
 
 # The full version, including alpha/beta/rc tags
 release = '0.9.1'
@@ -53,4 +53,4 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['source/_static']
